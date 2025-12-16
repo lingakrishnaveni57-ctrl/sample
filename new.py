d54f7f21e8017c9e9a81dccbdd9802d5c5f7f765
lingakrishnaveni57-ctrl <lingakrishnaveni57@gmail.com>
2025-12-16 13:32:44 +0000

    do fast

---
+++ new.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print("CI/CD pipeline is running successfully")
+    print("Add result:", add(5, 3))
+    print("Subtract result:", subtract(10, 4))
